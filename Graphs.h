31df172217cefd454776f91839279f35a005b494
prynshus <priyanshusengupta055@gmail.com>
2024-07-21 17:56:08 +0530

    Dijkstra algo

--- Graphs.h
+++ Graphs.h
@@ -4,66 +4,147 @@
 #include <vector>
 #include <queue>
 #include <stack>
+#include <climits>
 #include "LinkedLists.h"
+#include <bits/stdc++.h>
+#define INF 0x3f3f3f3f
 using namespace std;
+typedef pair<int,int> ipair;
 
 class adjMatrixGraph{
     int V;
     int E;
     int choice;
-    int* visited;
-    bool** adjMatrix;
+    bool* visited;
+    pair<bool,int>** adjMatrix;
     public:
         adjMatrixGraph(int V,int choice){
             if(V<0){throw invalid_argument("Too few Vertices.");}
             this->V=V;
             this->choice=choice;
             E=0;
-            adjMatrix= new bool*[V];
-            visited=new int[V];
-            for(int i=0;i<V;i++){adjMatrix[i]=new bool[V];}
+            visited=new bool[V];
+            adjMatrix= new pair<bool,int>*[V];
+            for(int i=0;i<V;i++){adjMatrix[i]=new pair<bool,int>[V];}
         }
 
-        void EdgeCreate(int V, int E){
-            if(E>(V*(V-1)/2)+V){throw invalid_argument("Too many edges.");}
-            if(E<0){throw invalid_argument("Too few edges.");}
-            random_device rd;
-            mt19937 gen(rd());
-            uniform_int_distribution<int> dis(0,V-1);
-            while(this->E!=E){
-                int u=dis(gen);
-                int v=dis(gen);
-                addEdge(u,v);
-            }
-        }
-
-        void clearVisited(){for(int i=0;i<V;i++){visited[i]=0;}}
+        void clearVisited(){for(int i=0;i<V;i++){visited[i]=false;}}
 
         int Vertices(){return V;}
         int Edges(){return E;}
+        
         void addEdge(int u,int v){
-            if(!adjMatrix[u][v]){
+            if(!adjMatrix[u][v].first){
                 E++;
-                adjMatrix[u][v]=true;
-                if(choice){adjMatrix[v][u]=true;}
+                adjMatrix[u][v].first=true;
+                if(choice){adjMatrix[v][u].first=true;}
+            }
+        }
+
+        void addEdge(int u,int v, int weight){
+            if(!adjMatrix[u][v].first){
+                E++;
+                adjMatrix[u][v].first=true;
+                adjMatrix[u][v].second=weight;
+                if(choice){
+                    adjMatrix[v][u].first=true;
+                    adjMatrix[v][u].second=weight;
+                }
             }
         }
 
         void removeEdge(int u,int v){
-            if(adjMatrix[u][v]){
+            if(adjMatrix[u][v].first){
                 E--;
-                adjMatrix[u][v]=false;
-                if(choice){adjMatrix[v][u]=false;}
+                adjMatrix[u][v].first=false;
+                if(choice){adjMatrix[v][u].first=false;}
             }
         }
 
-        bool checkEdge(int u,int v){return adjMatrix[u][v];}
+        bool checkEdge(int u,int v){return adjMatrix[u][v].first;}
         void DFS();
         void BFS();
-        void print();
-
-
+        void print(bool);
+        void Dijkstra(int);
+        void printReachSet(bool*,int*);
+        void DijkstraPQ(int);
 };
+
+void adjMatrixGraph::Dijkstra(int s){
+    int* dist=new int[V];
+    int* path= new int[V];
+    path[s]=s;
+    clearVisited();
+    visited[s]=true;
+    for(int i=0;i<V;i++){
+        if(adjMatrix[s][i].first){dist[i]=adjMatrix[s][i].second;}
+        else{dist[i]=INT_MAX;}
+    }
+    printReachSet(visited,dist);
+    for(int k=0;k<V-1;k++){
+        int minIndex;
+        int minDist=INT_MAX;
+        for(int n=0;n<V;n++){
+            if(!visited[n] && dist[n]<minDist){
+               minDist=dist[n];
+               minIndex=n;
+            }
+        }
+        visited[minIndex]=true;
+        for(int n=0;n<V;n++){
+            if(!visited[n] && adjMatrix[minIndex][n].first){
+                if(dist[minIndex] + adjMatrix[minIndex][n].second < dist[n]){
+                    dist[n]=dist[minIndex] + adjMatrix[minIndex][n].second;
+                    path[n]=minIndex;
+                }
+            }
+        }
+        printReachSet(visited,dist);
+    }
+    cout<<"Previous path of "<<s<<": "<<endl;
+    for(int i=0;i<V;i++){cout<<path[i]<<" ";}
+    delete[] dist;
+    delete[] path;
+}
+
+void adjMatrixGraph::printReachSet(bool* visited, int* dist){
+    cout<<"Visited set: ";
+    for(int i=0;i<V;i++){
+        if(visited[i]){cout<<i<<" ";}
+    }
+    cout<<endl;
+    cout<<"Distances:   ";
+    for(int i=0;i<V;i++){
+        if(dist[i]==INT_MAX){cout<<"- ";}
+        else{cout<<dist[i]<<" ";}
+    }
+    cout<<endl;
+}
+
+void adjMatrixGraph::DijkstraPQ(int s){
+    priority_queue<ipair,vector<ipair>,greater<ipair>> pq; //creates min heap
+    vector<int> dist(V,INF);
+    vector<int> path(V);
+    pq.push(make_pair(0,s));
+    dist[s]=0;
+    while(!pq.empty()){
+        int u=pq.top().second;
+        pq.pop();
+        for(int i=0;i<V;i++){
+            if(adjMatrix[u][i].first){
+                int weight=adjMatrix[u][i].second;
+                if(dist[i]>dist[u]+weight){dist[i]=dist[u] + weight;}
+                pq.push(make_pair(dist[i],i));
+                path[i]=u;
+            }
+        }
+    }
+    cout<<"Distances: "<<endl;
+    for(int i=0;i<V;i++){cout<<dist[i]<<" ";}
+    cout<<endl;
+    cout<<"Previous path: "<<endl;
+    for(int i=0;i<V;i++){cout<<path[i]<<" ";}
+}
 
 void adjMatrixGraph::DFS(){
     stack<int> s;
@@ -76,7 +157,7 @@ void adjMatrixGraph::DFS(){
             cout<<temp<<" ";
             visited[temp]=true;
             for(int i=0;i<V;i++){
-                if(adjMatrix[temp][i] && !visited[i]){s.push(i);}
+                if(adjMatrix[temp][i].first && !visited[i]){s.push(i);}
             }
         }
     }
@@ -93,20 +174,24 @@ void adjMatrixGraph::BFS(){
             cout<<temp<<" ";
             visited[temp]=true;
             for(int i=0;i<V;i++){
-                if(adjMatrix[temp][i] && !visited[i]){q.push(i);}
+                if(adjMatrix[temp][i].first && !visited[i]){q.push(i);}
             }
         }
     }
 }
 
-void adjMatrixGraph::print(){
+void adjMatrixGraph::print(bool choice){
     cout<<"  ";
     for(int i=0;i<V;i++){cout<<i<<" ";}
     cout<<endl;
     for(int i=0;i<V;i++){
         for(int j=0; j<V;j++){
             if(j==0){cout<<i<<" ";}
-            cout<<adjMatrix[i][j]<<" ";
+            if(!choice){cout<<adjMatrix[i][j].first<<" ";}
+            else{
+                if(adjMatrix[i][j].first){cout<<adjMatrix[i][j].second<<" ";}
+                else{cout<<"-"<<" ";}
+            }
         }
         cout<<endl;
     }
@@ -117,8 +202,8 @@ class adjListGraph{
     int E;
     int choice;
     int* visited;
-    vector<LinkedList*> adjList;
     public:
+        vector<LinkedList*> adjList;
         adjListGraph(int V,int choice){
             if(V<0){throw invalid_argument("Too few vertices.");}
             this->choice=choice;
@@ -199,3 +284,37 @@ void adjListGraph::BFS(){
     }
 }
 
+class graphOperations{
+    public:
+       void unweightedShortestPath(adjListGraph, int);
+};
+
+void graphOperations::unweightedShortestPath(adjListGraph g,int data){
+    int* dist= new int[g.Vertices()];
+    int* path= new int[g.Vertices()];
+    queue<int> q;
+    q.push(data);
+    for(int i=0;i<g.Vertices();i++){ path[i]=dist[i]=-1; }
+    dist[data]=0;
+    while(!q.empty()){
+        int v=q.front();
+        q.pop();
+        for(int w=0;w<g.Vertices();w++){
+            if(g.adjList[v]->find(w)){
+                if(dist[w]==-1){
+                    dist[w]=dist[v] + 1;
+                    path[w]=v;
+                    q.push(w);
+                }
+            }
+        }
+    }
+    cout<<"Distance of "<<data<<": "<<endl;
+    for(int i=0;i<g.Vertices();i++){cout<<dist[i]<<" ";}
+    cout<<endl;
+    cout<<"Previous vertex of "<<data<<": "<<endl;
+    for(int i=0;i<g.Vertices();i++){cout<<path[i]<<" ";}
+    cout<<endl;
+    delete[] path;
+    delete[] dist;
+}
